c2e206092e123f85fb40f217ac8bf320ffaea62b
MAIOTA <h20051007@gmail.com>
2025-11-27 09:39:25 +0900

    Initial commit

---
+++ src/test/java/work/maiotanet/MAIOTANetwork/MaiotaNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package work.maiotanet.MAIOTANetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaiotaNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
